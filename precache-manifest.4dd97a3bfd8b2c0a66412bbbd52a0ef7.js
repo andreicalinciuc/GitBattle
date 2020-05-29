bcdb63ca06be1601b62d837c332c5187d60cb65a
Andrei <andreic@sprybase.com>
2020-05-29 13:40:42 +0300

    Updates

---
+++ precache-manifest.4dd97a3bfd8b2c0a66412bbbd52a0ef7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e6e44bd8af368b2c91886fe03c2af0",
+    "url": "/GitBattle/index.html"
+  },
+  {
+    "revision": "01ce6920bf2f12b7efc7",
+    "url": "/GitBattle/static/css/2.34d4290a.chunk.css"
+  },
+  {
+    "revision": "713b9bf1044c51f0c864",
+    "url": "/GitBattle/static/css/main.f3017e4b.chunk.css"
+  },
+  {
+    "revision": "01ce6920bf2f12b7efc7",
+    "url": "/GitBattle/static/js/2.f4a14edb.chunk.js"
+  },
+  {
+    "revision": "ce1bfa9066abc63bb97af916637825fd",
+    "url": "/GitBattle/static/js/2.f4a14edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "713b9bf1044c51f0c864",
+    "url": "/GitBattle/static/js/main.a40882e7.chunk.js"
+  },
+  {
+    "revision": "d606ccafd4aff634baca",
+    "url": "/GitBattle/static/js/runtime-main.f9696880.js"
+  },
+  {
+    "revision": "60dfa264c766370831442b3aa14ef726",
+    "url": "/GitBattle/static/media/gitBattle.60dfa264.png"
+  }
+]);
